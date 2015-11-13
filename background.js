e803ed6638db890db75bc2ce2b7f3257f7af878a
Manuel Braun <thembrown@gmail.com>
2015-11-13 09:28:14 +0100

    Replace net polling with chrome.identity polling

--- background.js
+++ background.js
@@ -1,23 +1,18 @@
-var CHECK_URL = 'https://www.google.com/images/cleardot.gif?';
 
-function sendRequest() {
-  console.log('sending http request...');
-  var rnd = Math.random().toString(36).substr(2, 36);
-  var url = CHECK_URL + rnd;
-  fetch(url).then(function() {
-    console.log('request done');
-    scheduleRequest();
-  }, function(err) {
-    console.log('request failed');
-    scheduleRequest();
+function scheduleGetToken() {
+  // Not the proper way of doing it as event page might get unloaded in the
+  // meanwhile, but with the small timeout it's good enough for this demo.
+  window.setTimeout(getAuthToken, 2000);
+}
+
+function getAuthToken() {
+  console.log('getting token...');
+  chrome.identity.getAuthToken({interactive: false}, function(result) {
+    console.log('token result', result);
+    scheduleGetToken();
   });
 }
 
-function scheduleRequest() {
-  // Not the proper way of doing it as event page might get unloaded in the
-  // meanwhile, but with the small timeout it's good enough for this demo.
-  window.setTimeout(sendRequest, 2000);
-}
-
-sendRequest();
+// sendRequest();
+getAuthToken();
 
